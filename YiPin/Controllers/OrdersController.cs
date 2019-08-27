7cb8c7544306af86858660a0d4edd7a09fcf521b
Leon <liwangp2012@163.com>
2019-08-27 19:21:26 +0800

    2

--- YiPin/Controllers/OrdersController.cs
+++ YiPin/Controllers/OrdersController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using System.IO;
 namespace YiPin.Controllers
 {
     public class OrdersController : Controller
@@ -19,5 +19,31 @@ namespace YiPin.Controllers
             return View();
         }
 
+        [HttpPost]
+        public JsonResult ExtractArea(ExtractAreaModel model)
+        {
+            var tmpFolder = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "tmp");
+            if (!Directory.Exists(tmpFolder))
+                Directory.CreateDirectory(tmpFolder);
+            var orderFilePath = Path.Combine(tmpFolder, Guid.NewGuid().ToString() + ".xlsx");
+            if (model.OrderFile != null)
+                model.OrderFile.SaveAs(orderFilePath);
+
+
+
+
+
+
+
+            return Json(new { Name = "test" });
+        }
+
+    }
+
+
+    public class ExtractAreaModel
+    {
+        public string Area { get; set; }
+        public HttpPostedFileBase OrderFile { get; set; }
     }
 }
