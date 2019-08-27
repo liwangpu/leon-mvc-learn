5a91f7a8b7f52e41fa5b23d659dc3cc7e868dc35
Leon <liwangp2012@163.com>
2019-08-27 12:33:01 +0800

    1

---
+++ YiPin/Controllers/OrdersController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace YiPin.Controllers
+{
+    public class OrdersController : Controller
+    {
+        // GET: Orders
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ExtractArea()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ YiPin/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="YiPin.MvcApplication" Language="C#" %>
